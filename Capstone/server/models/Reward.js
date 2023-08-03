59e902d59977f46e9772a88541a5e2fca1bcf849
Q-Mick <quentinmick@protonmail.com>
2023-08-03 00:02:09 -0600

    update to tourny rewards

--- Capstone/server/models/Reward.js
+++ Capstone/server/models/Reward.js
@@ -1,28 +1,30 @@
 import { Schema } from "mongoose";
-const ObjectId = Schema.Types.ObjectId
+const ObjectId = Schema.Types.ObjectId;
 
-export const RewardSchema = new Schema({
-    tournamentId: { type: ObjectId, required: true, ref: 'Tournament'},
-    accountId: { type: ObjectId, required: false},
-    name: {type: String, required: true,},
-    badge: {type: String, required: true, },
-    gpCoins: {type: Number, required: true, default: 0},
-    recipientId: {type: ObjectId, ref: 'Participant', required: false}
-}, 
-    {timestamps: true, toJSON: {virtuals: true}})
+export const RewardSchema = new Schema(
+  {
+    tournamentId: { type: ObjectId, required: true, ref: "Tournament" },
+    accountId: { type: ObjectId, required: false },
+    name: { type: String, required: true },
+    badge: { type: String, required: true },
+    gpCoins: { type: Number, required: true, default: 0 },
+    recipientId: { type: ObjectId, ref: "Participant", required: false },
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
 
-    RewardSchema.index({ tournamentId: 1, accountId: 1}, { unique: true})
+RewardSchema.index({ accountId: 1, name: 1 }, { unique: true });
 
-RewardSchema.virtual('tournament', {
-    localField: 'tournamentId',
-    foreignField: '_id',
-    ref: 'Tournament',
-    justOne: true
-})
+RewardSchema.virtual("tournament", {
+  localField: "tournamentId",
+  foreignField: "_id",
+  ref: "Tournament",
+  justOne: true,
+});
 
-RewardSchema.virtual('participant', {
-    localField: 'recipientId',
-    foreignField: '_id',
-    ref: 'Participant',
-    justOne: true
-})
+RewardSchema.virtual("participant", {
+  localField: "recipientId",
+  foreignField: "_id",
+  ref: "Participant",
+  justOne: true,
+});
